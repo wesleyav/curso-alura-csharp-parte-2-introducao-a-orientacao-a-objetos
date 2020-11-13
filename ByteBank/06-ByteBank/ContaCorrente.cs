03f5b293083b11b4ad269f2ca9630f25c0e41c38
Wesley <wesley.contato@hotmail.com>
2020-11-13 06:28:25 -0300

    ObterSaldo e DefinirSaldo

---
+++ ByteBank/06-ByteBank/ContaCorrente.cs
@@ -0,0 +1,61 @@
+﻿// using _05_ByteBank;
+
+namespace _06_ByteBank
+{
+    public class ContaCorrente
+    {
+        public Cliente titular;
+        public int agencia;
+        public int numero;
+        private double saldo = 100;
+
+        public void DefinirSaldo(double saldo)
+        {
+            if (saldo < 0)
+            {
+                return;
+            }
+            else
+
+                this.saldo = saldo;
+
+        }
+
+        public double ObterSaldo()
+        {
+            return saldo;
+        }
+
+        public bool Sacar(double valor)
+        {
+            if (saldo < valor)
+            {
+                return false;
+            }
+
+            saldo -= valor;
+            return true;
+
+        }
+
+        public void Depositar(double valor)
+        {
+            saldo += valor;
+        }
+
+        public bool Transferir(double valor, ContaCorrente contaDestino)
+        {
+            if (this.saldo < valor)
+            {
+                return false;
+            }
+
+            saldo -= valor;
+            contaDestino.Depositar(valor);
+            return true;
+        }
+
+    }
+}
+
+
