9ef656c3c655b742add5ac504901c54030406bf8
Wesley <wesley.contato@hotmail.com>
2020-11-13 05:43:07 -0300

    Composicao de classes

--- ByteBank/05-ByteBank/Program.cs
+++ ByteBank/05-ByteBank/Program.cs
@@ -11,7 +11,22 @@ namespace _05_ByteBank
         static void Main(string[] args)
         {
             Cliente gabriela = new Cliente();
+            gabriela.nome = "Gabriela";
+            gabriela.profissao = "Desenvolvedora C#";
+            gabriela.cpf = "434.562.878-20";
 
+            ContaCorrente conta = new ContaCorrente();
+            conta.titular = gabriela;
+            conta.saldo = 500;
+            conta.agencia = 563;
+            conta.numero = 5634527;
+
+            conta.titular.nome = "Gabriela Costa";
+
+            Console.WriteLine(gabriela.nome);
+            Console.WriteLine(conta.titular.nome);
+
+            Console.ReadLine();
         }
     }
 }
